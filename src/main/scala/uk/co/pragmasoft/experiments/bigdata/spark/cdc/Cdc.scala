8cae5481ead43d0f020fdadd92806a159aaac5e5
galarragas <galarragas@gmail.com>
2015-05-25 17:21:23 +0100

    Unit Tests for CDC functions

--- src/main/scala/uk/co/pragmasoft/experiments/bigdata/spark/cdc/Cdc.scala
+++ src/main/scala/uk/co/pragmasoft/experiments/bigdata/spark/cdc/Cdc.scala
@@ -3,16 +3,23 @@ package uk.co.pragmasoft.experiments.bigdata.spark.cdc
 /**
  * Created by stefano on 26/04/15.
  */
+trait Cdc[+T] {
+  def operation: String
+  def record: T
+}
+
 object Cdc {
-   def inserted[T](record: T) = new Cdc(record, "I")
-   def deleted[T](record: T) = new Cdc(record, "D")
-   def updated[T](record: T) = new Cdc(record, "U")
-   def noop[T](record: T) = new Cdc(record, "")
+  private case class CdcImpl[+T](val record: T, val operation: String) extends Cdc[T]
 
-   def isNoop(cdc: Cdc[_]): Boolean = cdc.operation == ""
+  def inserted[T](record: T): Cdc[T] = new CdcImpl(record, "I")
+  def deleted[T](record: T): Cdc[T] = new CdcImpl(record, "D")
+  def updated[T](record: T): Cdc[T] = new CdcImpl(record, "U")
+  def noop[T](record: T): Cdc[T] = new CdcImpl(record, "")
+
+  def isNoop(cdc: Cdc[_]): Boolean = cdc.operation == ""
+
+  def printAsStringArray[T](cdc: Cdc[T])(implicit recordExtractor: T => Array[String]): Array[String] = recordExtractor(cdc.record) ++ Array(cdc.operation)
+}
 
 
-   def printAsStringArray[T](cdc: Cdc[T])(implicit recordExtractor: T => Array[String]): Array[String] = recordExtractor(cdc.record) ++ Array(cdc.operation)
- }
 
-class Cdc[T] private (val record: T, val operation: String) extends Serializable
---
+++ src/test/scala/uk/co/pragmasoft/experiments/bigdata/spark/cdc/CdcSupportTest.scala
@@ -0,0 +1,61 @@
+package uk.co.pragmasoft.experiments.bigdata.spark.cdc
+
+import org.scalatest.{Matchers, FlatSpec}
+import uk.co.pragmasoft.experiments.bigdata.spark.SparkTestSupport
+import Cdc._
+
+case class Record(key: Int, value: String)
+
+class CdcSupportTest extends FlatSpec with Matchers with SparkTestSupport with CdcSupport {
+
+  behavior of "extractDeletedRecords"
+
+  it should "extract as deleted record the ones with keys present in the previousSnapshot but not in the new one" in withSparkContext { sc =>
+
+    val previousSnapshot = sc.parallelize( List( Record(1, "value"), Record(2, "value 2"), Record(3, "value") ) ).keyBy( _.key )
+    val newSnapshot = sc.parallelize( List( Record(1, "new value"), Record(3, "value") ) ).keyBy( _.key )
+
+    val deletedRecords = extractDeletedRecords(newSnapshot, previousSnapshot)
+
+    deletedRecords.collect().toList should be ( List(deleted(Record(2, "value 2"))) )
+
+  }
+
+  behavior of "extractNewAndUpdated"
+
+  it should "Extract as NEW records the ones with keys not present in previous but present in new snapshot" in withSparkContext { sc =>
+    val previousSnapshot  = sc.parallelize( List( Record(1, "value"), Record(3, "value") ) ).keyBy( _.key )
+    val newSnapshot= sc.parallelize( List( Record(1, "value"), Record(2, "value 2"), Record(3, "value") ) ).keyBy( _.key )
+
+    val deletedRecords = extractNewAndUpdated(newSnapshot, previousSnapshot)
+
+    deletedRecords.collect().toList should be ( List(inserted(Record(2, "value 2"))) )
+
+  }
+
+  it should "Extract as UPDATED records the ones with keys present both snapshots but with different values and return new value" in withSparkContext { sc =>
+    val previousSnapshot  = sc.parallelize( List( Record(1, "value"), Record(3, "value") ) ).keyBy( _.key )
+    val newSnapshot= sc.parallelize( List( Record(1, "value"), Record(3, "new value") ) ).keyBy( _.key )
+
+    val deletedRecords = extractNewAndUpdated(newSnapshot, previousSnapshot)
+
+    deletedRecords.collect().toList should be ( List(updated(Record(3, "new value"))) )
+  }
+
+  behavior of "computeCdc"
+
+  it should "Detect updated, inserted and deleted records composing the other two functions, unaltered records should be ignored" in withSparkContext { sc =>
+    val record1 = Record(1, "value")
+    val record2 = Record(2, "value")
+    val record3 = Record(3, "value")
+    val record3Updated: Record = record3.copy(value = "new value")
+    val record4: Record = Record(4, "value")
+
+    val previousSnapshot  = sc.parallelize( List( record1, record2, record3 ) )
+    val newSnapshot= sc.parallelize( List( record1, record3Updated, record4 ) )
+
+    val cdc = computeCdc( (record: Record) => record.key )(newSnapshot, previousSnapshot)
+
+    cdc.collect().toSet should be ( Set( deleted(record2), updated(record3Updated), inserted(record4)  ) )
+  }
+}
