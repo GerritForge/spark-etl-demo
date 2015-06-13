b7d386af24f147724aa3d1832a2b5f0c6786c1c0
root <root@mesos3.gerritforge.com>
2015-06-13 23:50:15 +0100

    Changes the name of the images in order to get them from the docker registry

--- src/test/scripts/start-cdh-docker.sh
+++ src/test/scripts/start-cdh-docker.sh
@@ -5,4 +5,4 @@ ROOT_DIR=$2
 
 echo "Starting CDH - Root dir is $ROOT_DIR"
 
-docker run -d --link $ORACLE_NAME:$ORACLE_NAME -v $ROOT_DIR:/root/spark-etl cdh-juc > cdh.docker.id
+docker run -d --link $ORACLE_NAME:$ORACLE_NAME -v $ROOT_DIR:/root/spark-etl mesos3.gerritforge.com:5000/cdh-juc > cdh.docker.id
